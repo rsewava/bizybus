d6f3068a875510fe19bd8a13f6e35243f02b1b25
Rodney Sewava Isaac Tajir Mattar <r.sewava@gmail.com>
2022-04-04 04:10:33 +0300

    Add promotional div.

--- index.php
+++ index.php
@@ -10,52 +10,69 @@
 		<meta 	charset	=	"UTF-8" />
 	</head>
 	<body>
-		<nav	class	=	"navbar">
-			<!-- LOGO -->
-			<div	class	=	"logo">Bizybus</div>
-			<!-- NAVIGATION MENU -->
-			<ul		class	=	"nav-links">
-				<!-- USING CHECKBOX HACK -->
-				<input	id		=	"checkbox_toggle" 
-						type	=	"checkbox"/>
-				<label	class	=	"hamburger"
-						for		=	"checkbox_toggle">
-					&#9776;
-				</label>
-				<!-- NAVIGATION MENUS -->
-				<div	class	=	"menu">
-					<li>
-						<a		href	=	"/">Home</a>
-					</li>
-					<li>
-						<a		href	=	"/">About Us</a>
-					</li>
-					<li		class	=	"services">
-						<a		href	=	"/">Services</a>
-						<!-- DROPDOWN MENU -->
-						<ul		class	=	"dropdown">
-							<li>
-								<a		href	=	"/">Dropdown 1</a>
-							</li>
-							<li>
-								<a		href	=	"/">Dropdown 2</a>
-							</li>
-							<li>
-								<a		href	=	"/">Dropdown 3</a>
-							</li>
-							<li>
-								<a		href	=	"/">Dropdown 4</a>
-							</li>
-						</ul>
-					</li>
-					<li>
-						<a		href	=	"/">Pricing</a>
-					</li>
-					<li>
-						<a		href	=	"/">Contact Us</a>
-					</li>
-				</div>
-			</ul>
-		</nav>
+		<header>
+
+			<nav	class	=	"navbar">
+				<!-- LOGO -->
+				<div	class	=	"logo">Bizybus</div>
+				<!-- NAVIGATION MENU -->
+				<ul		class	=	"nav-links">
+					<!-- USING CHECKBOX HACK -->
+					<input	id		=	"checkbox_toggle" 
+							type	=	"checkbox"/>
+					<label	class	=	"hamburger"
+							for		=	"checkbox_toggle">
+						&#9776;
+					</label>
+					<!-- NAVIGATION MENUS -->
+					<div	class	=	"menu">
+						<li>
+							<a		href	=	"/">Home</a>
+						</li>
+						<li>
+							<a		href	=	"/">About Us</a>
+						</li>
+						<li		class	=	"services">
+							<a		href	=	"/">Services</a>
+							<!-- DROPDOWN MENU -->
+							<ul		class	=	"dropdown">
+								<li>
+									<a		href	=	"/">Bus Ticketing</a>
+								</li>
+								<li>
+									<a		href	=	"/">Parcel Delivery</a>
+								</li>
+								<li>
+									<a		href	=	"/">Savings</a>
+								</li>
+								<li>
+									<a		href	=	"/">Hotel Bookings</a>
+								</li>
+							</ul>
+						</li>
+						<li>
+							<a		href	=	"/">Pricing</a>
+						</li>
+						<li>
+							<a		href	=	"/">Contact Us</a>
+						</li>
+					</div>
+				</ul>
+			</nav>
+		</header>
+<?php
+$total_visits
+?>
+		<main>
+			<div>
+				Stop Looking.
+				Start Tracking!
+				The fututre of bus transport in Uganda starts with Bizybus.
+				We are Uganda's Largest online bus booking platform and are trusted by over x users. 
+			</div>
+		</main>
+		<footer>
+			
+		</footer>
 	</body>
 </html>
